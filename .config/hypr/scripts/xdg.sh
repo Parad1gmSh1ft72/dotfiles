324d98d6acfb8057e622236337d4dad46c57e0a6
parad1gmsh1ft72 <charlescravens@gmail.com>
2025-03-14 10:30:37 -0500

    new

--- .config/hypr/scripts/xdg.sh
+++ .config/hypr/scripts/xdg.sh
@@ -48,8 +48,8 @@ fi
 sleep $_sleep2
 
 # Start required services
-systemctl --user start pipewire
-systemctl --user start wireplumber
+# systemctl --user start pipewire
+# systemctl --user start wireplumber
 systemctl --user start xdg-desktop-portal
 # systemctl --user start xdg-desktop-portal-hyprland
 
